e2ab5cd47fc400c0a4cbe503a083e7f718f20853
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2025-04-04 18:01:32 +0200

    feat: initialize project with Unlighthouse integration

---
+++ unlighthouse.config.ts
@@ -0,0 +1,15 @@
+export default {
+  site: "esim.holafly.com",
+  urls: [
+    "/"
+  ],
+  ci: {
+    budget: {
+      "performance": 50,
+      "accessibility": 80,
+      "best-practices": 80,
+      "seo": 80,
+    },
+    buildStatic: true,
+  }
+}
